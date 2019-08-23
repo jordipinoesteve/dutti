57f09f199fa7d89149079ea5248f596d4f362f06
jordipinoesteve <jordipinoesteve@gmail.com>
2019-08-23 19:47:08 +0200

    Diseño pantalla de ships, autocarga de ships en scroll, modificaciones pantallas login/registro.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app', ['ngRoute', 'ngCookies'])
+        .module('app', ['ngRoute', 'ngCookies','angular.filter','ngAnimate'])
         .config(config)
         .run(run);
 
--- ships/ships.controller.js
+++ ships/ships.controller.js
@@ -7,9 +7,24 @@
 
     ShipsController.$inject = ['ShipsService', '$scope','$cookies'];
     function ShipsController(ShipsService,$scope,$cookies) {
+
+        window.scrollTo(0, 0);
+
+        var windowscroll = function (){
+          if($(window).scrollTop() == 0){
+              return
+          }
+          if($(window).scrollTop() >= ($(document).height() - 50) - $(window).height()){
+                $(window).unbind('scroll',windowscroll)
+                $("#next").click();
+
+            }
+        }
+
         var _this = this;
         var cancel = false; //Variable de control cancelar en caso de que no hayan más naves.
         var page = 1;
+
         _this.fetchNext = function ()  {
 
             if (cancel) {return}; //Cancelar llamada en caso de que no hayan más naves para mostrar.
@@ -20,6 +35,7 @@
             if($cookies.get("localAPI" + page)){
                 _this.fillData(JSON.parse(localStorage.getItem("page" + page)));
             }else{
+                $("#loading").attr("style","display:block");
                 ShipsService.GetStarships(url)
                     .then(function (data) {
 
@@ -30,11 +46,13 @@
                         _this.setTimer(data,page)
 
                         _this.fillData(data)
+                        $("#loading").attr("style","display:none");
                     })
                     .catch(function () {
                         // Si falla la API, comprobar si tiene datos en localstorage aunque la cookie esté caducada, considerando que es mejor mostrar datos a un mensaje de error.
                         if(localStorage.getItem("page" + page)){
                              _this.fillData(JSON.parse(localStorage.getItem("page" + page)));
+                             $("#loading").attr("style","display:none");
                         }else{
                             _this.error = true;
                         }
@@ -50,7 +68,6 @@
         }
 
         _this.fillData = function(data){
-            console.log(data)
             //Evitar naves duplicadas en la vista en el caso de que se hagan las peticiones a API muy seguidas.
             if (_this.lastResponse.next != data.next){
                 _this.starships = _this.starships.concat(data.results);
@@ -62,8 +79,8 @@
             if(data.next == null){
                 cancel = true; //cancelar más llamadas a API.
             }
-
             $scope.$digest;
+            $(window).on('scroll', windowscroll)
         }
 
         _this.setTimer = function(data,page){
