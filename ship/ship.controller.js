a3b4d9e060a663abfd2077e0ddfd9cb52446a7b7
jordipinoesteve <jordipinoesteve@gmail.com>
2019-08-24 17:59:01 +0200

    Correcciones autocarga scroll rápido, comentarios, limpieza de código y corrección div de errores.

--- ship/ship.controller.js
+++ ship/ship.controller.js
@@ -5,28 +5,27 @@
         .module('app')
         .controller('ShipController', ShipController);
 
-    ShipController.$inject = ['ShipsService', '$scope', '$cookies','$routeParams'];
+    ShipController.$inject = ['ShipsService', '$scope', '$cookies','$routeParams','$location'];
 
-    function ShipController(ShipsService, $scope, $cookies,$routeParams) {
+    function ShipController(ShipsService, $scope, $cookies,$routeParams,$location) {
 
         var _this = this;
 
         _this.fetchInfo = function () {
             $("#loading2").attr("style", "display:block;");
+
+            //Método de lectura de nave pasando parámetro id
             ShipsService.ReadStarShip($routeParams.id)
                 .then(function (data) {
                     _this.starship = data;
                     $("#loading2").attr("style", "display:none;");
-                    $scope.$digest();
-
                 })
-                .catch(function () {
-                    _this.error = true;
-                    $scope.$digest();
+                .catch(function (err) {
+                    //Si la nave no existe, por ejemplo, accediendo a traves de url con un id inexistente, redirige a ships.
+                    $location.url("/ships")
                     $("#loading2").attr("style", "display:none;");
                 })
         }
-        _this.error = undefined;
         _this.lastResponse = {};
         _this.starship = {};
         _this.fetchInfo();
--- ships/ships.controller.js
+++ ships/ships.controller.js
@@ -1,95 +1,105 @@
 (function () {
-    'use strict';
+        'use strict';
 
-    angular
-        .module('app')
-        .controller('ShipsController', ShipsController);
+        angular
+            .module('app')
+            .controller('ShipsController', ShipsController);
 
-    ShipsController.$inject = ['ShipsService', '$scope', '$cookies'];
+        ShipsController.$inject = ['ShipsService', '$scope', '$cookies'];
 
-    function ShipsController(ShipsService, $scope, $cookies) {
+        function ShipsController(ShipsService, $scope, $cookies) {
 
-        window.scrollTo(0, 0);
+            window.scrollTo(0, 0); //Empezar siempre listado desde arriba.
 
-        var currentHeight =  angular.copy($(document).height().valueOf())
+            var windowscroll = function () {
+                if ($(window).scrollTop() == 0 || ($(document).height()) - $(window).height() < 0) {
+                    return
+                }
+                if ($(window).scrollTop() >= ($(document).height()) - $(window).height()) {
+                    $(window).off('scroll', windowscroll)
+                    $("#next").click();
 
-        var windowscroll = function () {
-            if ($(window).scrollTop() == 0 || ($(document).height()) - $(window).height() < 0) {
-                return
+                    if($("#next").html() == undefined){
+                        $(window).on('scroll', windowscroll) //caso en que el botón no esté disponible
+                    }
+
+
+                }
             }
-            if ($(window).scrollTop() >= ($(document).height()) - $(window).height()) {
-                $(window).off('scroll', windowscroll)
-                $("#next").click();
 
-            }
-        }
+            var _this = this;
+            var cancel = false; //Variable de control cancelar en caso de que no hayan más naves.
+            var page = 1;
 
-        var _this = this;
-        var cancel = false; //Variable de control cancelar en caso de que no hayan más naves.
-        var page = 1;
+            _this.fetchNext = function () {
+                console.log(1)
+                if (cancel) {
+                    return
+                }; //Cancelar llamada en caso de que no hayan más naves para mostrar.
 
-        _this.fetchNext = function () {
+                console.log(2)
+                var url = _this.lastResponse ? _this.lastResponse.next : null;
 
-            if (cancel) {
-                return
-            }; //Cancelar llamada en caso de que no hayan más naves para mostrar.
+                if ($cookies.get("localAPI" + page)) {
+                    console.log(3)
+                    _this.fillData(JSON.parse(localStorage.getItem("page" + page)));
 
+                } else {
+                    console.log(4)
+                    $("#loading").attr("style", "display:block");
+                    ShipsService.GetStarships(url)
+                        .then(function (data) {
 
-            var url = _this.lastResponse ? _this.lastResponse.next : null;
+                            //Borrar localstorage en caso de que la llamada sea exitosa.
+                            localStorage.removeItem("page" + page);
 
-            if ($cookies.get("localAPI" + page)) {
-                _this.fillData(JSON.parse(localStorage.getItem("page" + page)));
-            } else {
-                $("#loading").attr("style", "display:block");
-                ShipsService.GetStarships(url)
-                    .then(function (data) {
+                            //Guardar cookie de caducidad 5 minutos para no atacar constantemente a la API.
+                            _this.setTimer(data, page)
 
-                        //Borrar localstorage en caso de que la llamada sea exitosa.
-                        localStorage.removeItem("page" + page);
-
-                        //Guardar cookie de caducidad 5 minutos para no atacar constantemente a la API.
-                        _this.setTimer(data, page)
-
-                        _this.fillData(data)
-                        $("#loading").attr("style", "display:none");
-                    })
-                    .catch(function () {
-                        // Si falla la API, comprobar si tiene datos en localstorage aunque la cookie esté caducada, considerando que es mejor mostrar datos a un mensaje de error.
-                        if (localStorage.getItem("page" + page)) {
-                            _this.fillData(JSON.parse(localStorage.getItem("page" + page)));
+                            _this.fillData(data)
                             $("#loading").attr("style", "display:none");
-                        } else {
-                            _this.error = true;
-                        }
+                        })
+                        .catch(function () {
+                            // Si falla la API, comprobar si tiene datos en localstorage aunque la cookie esté caducada, considerando que es mejor mostrar datos a un mensaje de error.
+                            if (localStorage.getItem("page" + page)) {
+                                _this.fillData(JSON.parse(localStorage.getItem("page" + page)));
+                                $("#loading").attr("style", "display:none");
+                            } else {
+                                _this.error = true;
+                                $("#loading").attr("style", "display:none");
+                            }
 
 
-                        $scope.$digest();
-                    })
+                            $scope.$digest();
+                        })
+                }
+
+
+
+
             }
 
+            _this.fillData = function (data) {
+                //Evitar naves duplicadas en la vista en el caso de que se hagan las peticiones a API muy seguidas.
+                if (_this.lastResponse.next != data.next) {
+                    _this.starships = _this.starships.concat(data.results);
+                    _this.lastResponse = data;
+                    page = page + 1;
+                }
+
+                console.log(data.next, cancel)
+                if (data.next == null) {
+                    cancel = true; //cancelar más llamadas a API.
+
+                }
+
+                $scope.$digest;
 
 
+                window.scrollTo(0, $(document).height() - $(window).height() - 10)
 
-        }
-
-        _this.fillData = function (data) {
-            //Evitar naves duplicadas en la vista en el caso de que se hagan las peticiones a API muy seguidas.
-            if (_this.lastResponse.next != data.next) {
-                _this.starships = _this.starships.concat(data.results);
-                _this.lastResponse = data;
-                page = page + 1;
-            }
-
-
-            if (data.next == null) {
-                cancel = true; //cancelar más llamadas a API.
-            }
-            $scope.$digest;
-
-
-            window.scrollTo(0, $(document).height() - $(window).height() - 10)
-
-            $(window).on('scroll', windowscroll)
+                $(window).on('scroll', windowscroll)
+                console.log('on')
 
         }
 
@@ -106,7 +116,7 @@
         _this.starships = [];
 
 
-
+        $(window).off('scroll', windowscroll)
         _this.fetchNext();
 
     }
